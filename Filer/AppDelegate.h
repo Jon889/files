d08d4e9b795233de5f8272d51f6658b1b78e5305
Jonathan Bailey <jonathan@dyn1205-192.wlan.ic.ac.uk>
2015-01-04 16:20:03 +0000

    Initial Commit

---
+++ Filer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Filer
+//
+//  Created by Jonathan on 04/01/2015.
+//  Copyright (c) 2015 Jonathan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
